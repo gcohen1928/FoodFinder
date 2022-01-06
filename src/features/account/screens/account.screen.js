f904b68ef37012166faaa296cc05f0874ce3500b
gcohen1928 <gabriel.h.cohen@vanderbilt.edu>
2022-01-06 15:49:48 -0500

    App 2.0

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -2,18 +2,28 @@ import React from "react";
 import { AccountBackground, AccountCover, AccountContainer, AuthButton, Title } from "../components/account.styles";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import LottieView from 'lottie-react-native'
+import styled from "styled-components";
 
+const LottieContainer = styled.View`
+    position: absolute
+    margin-top: 50px
+    margin-left: 50px
+`
 
 export const AccountScreen = ({ navigation }) => {
     return (
 
         <AccountBackground>
             <AccountCover />
-            <LottieView
-              source={require("../../../../assets/watermelon.json")}
-                autoPlay
-                loop
-            />
+            
+                <LottieView
+                    source={require("../../../../assets/watermelon.json")}
+                    autoPlay
+                    loop
+                    style={{marginBottom: 250}}
+                />
+           
+
             <Title>FoodFinder</Title>
             <AccountContainer>
                 <AuthButton icon="lock-open-outline"
